b340f8a20a75632a96c9bd9b75b5cdcabba4bbb5
Mark Smith <msmith4@gmail.com>
2016-07-23 17:39:16 -0600

    Added reactor values, and auto-shutdown.

--- power.lua
+++ power.lua
@@ -3,94 +3,123 @@ local term      = require("term")
 local keyboard  = require("keyboard")
 local format    = require("format")
 
-local gpu    = component.gpu
-local matrix = component.induction_matrix
-
-local oldW, oldH = gpu.getResolution()
-gpu.setResolution(40,10)
+local gpu     = component.gpu
+local matrix  = component.induction_matrix
+local reactor = component.reactor_logic_adapter
 
 local function toRf(joules)
   return joules/2.5
 end
 
-local function getEnergyRf()
-  return toRf(matrix.getEnergy())
+local function getMatrixEnergy()
+  return format.formatNumber(toRf(matrix.getEnergy()))
 end
 
-local function getMaxEnergyRf()
-  return toRf(matrix.getMaxEnergy())
+local function getMatrixMaxEnergy()
+  return format.formatNumber(toRf(matrix.getMaxEnergy()))
 end
 
-local function getPercentFull()
-  return (matrix.getEnergy()/matrix.getMaxEnergy())*100
+local function getMatrixEnergyPercent()
+  return format.formatNumber((matrix.getEnergy()/matrix.getMaxEnergy())*100)
 end
 
-local function getInputRf()
-  return toRf(matrix.getInput())
+local function getMatrixInputRate()
+  return format.formatNumber(toRf(matrix.getInput()))
 end
 
-local function getInputPercent()
-  return (matrix.getInput()/matrix.getTransferCap())*100
+local function getMatrixInputPercent()
+  return format.formatNumber((matrix.getInput()/matrix.getTransferCap())*100)
 end
 
-local function getOutputRf()
-  return toRf(matrix.getOutput())
+local function getMatrixOutputRate()
+  return format.formatNumber(toRf(matrix.getOutput()))
 end
 
-local function getOutputPercent()
-  return (matrix.getOutput()/matrix.getTransferCap())*100
+local function getMatrixOutputPercent()
+  return format.formatNumber((matrix.getOutput()/matrix.getTransferCap())*100)
 end
 
-local function getTransferCapRf()
-  return toRf(matrix.getTransferCap())
+local function getMatrixTransferCap()
+  return format.formatNumber(toRf(matrix.getTransferCap()))
+end
+
+local function getReactorPlasmaHeatPercent()
+  if reactor.getMaxPlasmaHeat() > 0 then
+    return format.formatNumber((reactor.getPlasmaHeat()/reactor.getMaxPlasmaHeat())*100) 
+  else 
+    return 0
+  end
+end
+
+local function getReactorProducing()
+  return format.formatNumber(toRf(reactor.getProducing()))
+end
+
+local function getReactorStatus()
+  if reactor.isIgnited() then
+    return "Online"
+  else
+    return "Offline"
+  end
+end  
+
+local function handleMatrixFull()
+  if matrix.getEnergy() == matrix.getMaxEnergy() then
+    if reactor.isIgnited() then
+      reactor.setInjectionRate(0)
+    end
+  end
 end
 
 
-local maxEnergy
-local energy
-local percentFull
-local input
-local inputPercent
-local output
-local outputPercent
-local transferCap
+local matrixMaxEnergy, matrixEnergy, matrixEnergyPercent
+local matrixInputRate, matrixInputPercent
+local matrixOutputRate, matrixOutputPercent, matrixTransferCap
+local reactorStatus, reactorPlasmaHeatPercent, reactorProducing
 
 local function setEnergyValues()
-  maxEnergy     = format.formatNumber(getMaxEnergyRf())
-  energy        = format.formatNumber(getEnergyRf())
-  percentFull   = format.formatNumber(getPercentFull())
-  input         = format.formatNumber(getInputRf())
-  inputPercent  = format.formatNumber(getInputPercent())
-  output        = format.formatNumber(getOutputRf())
-  outputPercent = format.formatNumber(getOutputPercent())
-  transferCap   = format.formatNumber(getTransferCapRf())
+  matrixMaxEnergy     = getMatrixMaxEnergy()
+  matrixEnergy        = getMatrixEnergy()
+  matrixEnergyPercent = getMatrixEnergyPercent()
+  matrixInputRate     = getMatrixInputRate()
+  matrixInputPercent  = getMatrixInputPercent()
+  matrixOutputRate    = getMatrixOutputRate()
+  matrixOutputPercent = getMatrixOutputPercent()
+  matrixTransferCap   = getMatrixTransferCap()
+
+  reactorStatus            = getReactorStatus()
+  reactorPlasmaHeatPercent = getReactorPlasmaHeatPercent()
+  reactorProducing         = getReactorProducing()
 end
 
 local function printStatus()
-  print("Max Capacity:      "..maxEnergy.." RF")
-  print("Current Capacity:  "..energy.." RF ("..percentFull.."%)")
+  print("Max Capacity:      "..matrixMaxEnergy.." RF")
+  print("Current Capacity:  "..matrixEnergy.." RF ("..matrixEnergyPercent.."%)")
   print()
-  print("Max Transfer Rate: "..transferCap.." RF/t")
-  print("Input Rate:        "..input.." RF/t".." ("..inputPercent.."%)")
-  print("Output Rate:       "..output.." RF/t".." ("..outputPercent.."%)")
+--  print("Max Transfer Rate: "..matrixTransferCap.." RF/t")
+  print("Input Rate:        "..matrixInputRate.." RF/t".." ("..matrixInputPercent.."%)")
+  print("Output Rate:       "..matrixOutputRate.." RF/t".." ("..matrixOutputPercent.."%)")
+  print()
+  print("Reactor Status:    "..reactorStatus)
+  print("Reactor Heat:      "..reactorPlasmaHeatPercent.."%")
+  print("Reactor Power:     "..reactorProducing.." RF/t")
 end
 
-term.clear()
-setEnergyValues()
-printStatus()
+local oldMatrixEnergy, oldMatrixInputRate, oldMatrixOutputRate
+
+local oldW, oldH = gpu.getResolution()
+gpu.setResolution(40, 10)
 
 while true do
-  local oldEnergy = energy
-  local oldInput  = input
-  local oldOutput = output
   setEnergyValues()
-  if oldEnergy ~= energy or oldInput ~= input or oldOutput ~= output then
-    term.clear()
-    printStatus()
-  end
+  term.clear()
+  printStatus()
+  handleMatrixFull()
+
   if keyboard.isControlDown() and keyboard.isKeyDown(keyboard.keys.w) then
     gpu.setResolution(oldW, oldH)
     os.exit()
   end
+
   os.sleep(0.5)
 end

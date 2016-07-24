b83a5368f6904bed9ed05459997258ebeb195154
Mark Smith <msmith4@gmail.com>
2016-07-24 17:48:04 -0600

    Bug fixes, and added monochrome support

--- lib/ui.lua
+++ lib/ui.lua
@@ -1,73 +1,88 @@
 local ui = {}
-
+ 
 local component = require("component")
 local gpu       = component.gpu
-
+ 
 local screenWidthOuter, screenHeightOuter
 local screenWidthInner, screenHeightInner
 local screenTopInner, screenLeftInner
-
+local maxDepth
+ 
 local function scaleValue(n1, n1Max, n2Max)
-  return math.floor((n2Max*n1)/n1Max)
+  if n1Max > 0 then
+    return math.floor((n2Max*n1)/n1Max)
+  end
+  return 0
 end  
+
+local function setBackground(color, isIndex, monoColor)
+  monoColor = monoColor or 0x000000
+  if maxDepth < 4 then
+    color   = monoColor
+    isIndex = false
+  end
+  local oldColor = gpu.setBackground(color, isIndex)
+  return oldColor
+end
 
 function ui.clear()
   local w, h = gpu.getResolution()
   gpu.fill(1, 1, w, h, " ")
 end
-
+ 
 function ui.setScreenPaddingPercent(top, right, bottom, left)
   local topOffset    = math.floor(screenHeightOuter*(top   /100))
   local rightOffset  = math.floor(screenWidthOuter *(right /100))
   local bottomOffset = math.floor(screenHeightOuter*(bottom/100))
   local leftOffset   = math.floor(screenWidthOuter *(left  /100))
-  
+ 
   screenTopInner    = topOffset+1
   screenLeftInner   = leftOffset+1
   screenWidthInner  = screenWidthOuter-rightOffset-leftOffset
   screenHeightInner = screenHeightOuter-topOffset-bottomOffset
 end
-
+ 
 function ui.getPanelDimensions(panelCount)
   local panels = {}
   local panelBottomPrevious = screenTopInner
   local panelHeight = math.floor(screenHeightInner/panelCount)
-  
-  for local i = 0, panelCount - 1 do
+ 
+  for i = 0, panelCount - 1 do
     panels[i]           = {}
-    panels[i]{"x"}      = screenLeftInner
-    panels[i]{"y"}      = panelBottomPrevious
-    panels[i]{"w"}      = screenWidthInner
-    panels[i]{"h"}      = panelHeight
-    panels[i]{"middle"} = panelBottomPrevious+math.floor(panelHeight/2)
-    panelBottomPrevious = panel[i]{"y"} + panel[i]{"h"}
-  end 
+    panels[i]["x"]      = screenLeftInner
+    panels[i]["y"]      = panelBottomPrevious
+    panels[i]["w"]      = screenWidthInner
+    panels[i]["h"]      = panelHeight
+    panels[i]["middle"] = panelBottomPrevious+math.floor(panelHeight/2)
+    panelBottomPrevious = panels[i]["y"] + panels[i]["h"]
+  end
   return panels
 end
-
+ 
 function ui.drawPanelBackground(panel, color, isIndex)
-  local oldColor = gpu.setBackground(color, isIndex)
-  gpu.fill(panel{"x"}, panel{"y"}, panel{"w"}, panel{"h"}, " ")
+  local oldColor = setBackground(color, isIndex)
+  gpu.fill(panel["x"], panel["y"], panel["w"], panel["h"], " ")
   gpu.setBackground(oldColor)
 end
-
+ 
 function ui.drawHorizontalPercentageBar(x, y, height, fgColor, isFgIndex, bgColor, isBgIndex, val, maxVal, width)
   local innerWidth = scaleValue(val, maxVal, width)
- 
-  local oldColor = gpu.setBackground(bgColor, isBgIndex)
+
+  local oldColor = setBackground(bgColor, isBgIndex)
   gpu.fill(x, y, width, height, " ")
-
-  gpu.setBackground(fgColor, ifFgIndex)
+ 
+  setBackground(fgColor, isFgIndex, 0xFFFFFF)
   gpu.fill(x, y, innerWidth, height, " ")
-
-  gpu.setBackground(oldBg)
+ 
+  gpu.setBackground(oldColor)
 end
-
-
+ 
+ 
 screenWidthOuter, screenHeightOuter = gpu.getResolution()
 screenWidthInner  = screenWidthOuter
 screenHeightInner = screenHeightOuter
 screenTopInner    = 1
-screenLeftInner   = 1 
-
+screenLeftInner   = 1
+maxDepth          = gpu.maxDepth()
+ 
 return ui
--- ui-test.lua
+++ ui-test.lua
@@ -8,11 +8,10 @@ local panels = ui.getPanelDimensions(panelCount)
 
 ui.clear()
 
-ui.drawPanelBackground(panels[0], colors.white)
-ui.drawPanelBackground(panels[1], colors.yellow)
-ui.drawPanelBackground(panels[2], colors.pink)
+ui.drawPanelBackground(panels[0], colors.white, true)
+ui.drawPanelBackground(panels[1], colors.yellow, true)
+ui.drawPanelBackground(panels[2], colors.pink, true)
 
-ui.drawHorizontalPercentageBar(panels[0]{"x"}, panels[0]{"middle"}, 1, colors.black, true, colors.gray, true, 20, 50, panels[0]{"w"})
-ui.drawHorizontalPercentageBar(panels[1]{"x"}, panels[1]{"middle"}, 1, colors.red, true, colors.gray, true, 50, 100, panels[1]{"w"})
-ui.drawHorizontalPercentageBar(panels[2]{"x"}, panels[2]{"middle"}, 1, colors.lightblue, true, colors.gray, true, 80, 100, panels[2]{"w"})
-
+ui.drawHorizontalPercentageBar(panels[0]["x"], panels[0]["middle"], 1, colors.black, true, colors.gray, true, 20, 50, panels[0]["w"])
+ui.drawHorizontalPercentageBar(panels[1]["x"], panels[1]["middle"], 1, colors.red, true, colors.gray, true, 50, 100, panels[1]["w"])
+ui.drawHorizontalPercentageBar(panels[2]["x"], panels[2]["middle"], 1, colors.lightblue, true, colors.gray, true, 80, 100, panels[2]["w"])

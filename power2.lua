e2f8915092dc886062344abfa1e235c99e6001e3
Mark Smith <msmith4@gmail.com>
2016-07-26 20:57:41 -0600

    Refactored.  Wrapped component calls into pcall()s

--- power2.lua
+++ power2.lua
@@ -4,11 +4,9 @@ local format    = require("format")
 local ui        = require("ui")
 local colors    = require("colors")
 
-local matrix  = component.induction_matrix
-local reactor = component.reactor_logic_adapter
-
 local function toRf(joules)
   joules = tonumber(joules) or 0 
+
   return joules/2.5
 end
 
@@ -17,8 +15,9 @@ local function formatNumber(n)
 end
 
 local function formatPercent(n, max)
-  n   = tonumber(n) or 0
+  n   = tonumber(n)   or 0
   max = tonumber(max) or 0
+
   if max > 0 then
     return format.formatNumber((n/max)*100)
   else
@@ -26,28 +25,40 @@ local function formatPercent(n, max)
   end
 end
 
-local function getReactorStatus()
-  if reactor.isIgnited() then
-    return "Online"
-  else
-    return "Offline"
-  end
-end  
+local function getInductionMatrixValues()
+  local matrix = {}
 
-local function handleMatrixFull()
-  if matrix.getEnergy() == matrix.getMaxEnergy() then
-    if reactor.isIgnited() then
-      reactor.setInjectionRate(0)
-    end
-  end
+  local status, err = pcall(function ()
+    local compMatrix = component.induction_matrix
+    matrix["maxEnergy"]   = compMatrix.getMaxEnergy()
+    matrix["energy"]      = compMatrix.getEnergy()
+    matrix["input"]       = compMatrix.getInput()
+    matrix["output"]      = compMatrix.getOutput()
+    matrix["transferCap"  = compMatrix.getTransferCap()
+  end)
+
+  return matrix
 end
 
+local function getReactorLogicAdapterValues()
+  local reactor = {}
 
-local matrixMaxEnergy, matrixEnergy, matrixEnergyPercent
-local matrixInputRate, matrixInputPercent
-local matrixOutputRate, matrixOutputPercent, matrixTransferCap
-local reactorStatus, reactorPlasmaHeatPercent, reactorProducing
-local scaledTransferCap
+  local status, err = pcall(function ()
+    local compReactor = component.reactor_logic_adapter
+    reactor["isIgnited"]     = compReactor.isIgnited()
+    reactor["plasmaHeat"]    = compReactor.getPlasmaHeat()
+    reactor["maxPlasmaHeat"] = compReactor.getMaxPlasmaHeat()
+    matrix["producing"]      = compMatrix.getProducing()
+  end)
+
+  return reactor
+end
+
+local function powerReactorOff()
+    local status, err = pcall(function ()
+      reactor.setInjectionRate(0)
+    end)
+end
 
 local function getScaledTransferCap(input, output, cap)
   input  = tonumber(input)  or 0
@@ -64,24 +75,30 @@ local function getScaledTransferCap(input, output, cap)
   return cap
 end
 
-local function setEnergyValues()
-  matrixMaxEnergy     = formatNumber(matrix.getMaxEnergy())
-  matrixEnergy        = formatNumber(matrix.getEnergy())
-  matrixEnergyPercent = formatPercent(matrix.getEnergy(), matrix.getMaxEnergy())
-  matrixInputRate     = formatNumber(matrix.getInput())
-  matrixInputPercent  = formatPercent(matrix.getInput(), matrix.getTransferCap())
-  matrixOutputRate    = formatNumber(matrix.getOutput())
-  matrixOutputPercent = formatPercent(matrix.getOutput(), matrix.getTransferCap())
-  matrixTransferCap   = formatNumber(matrix.getTransferCap())
+local function getMatrixReactorStatus()
+  local matrix  = getInductionMatrixValues()
+  local reactor = getReactorLogicAdapterValues()
+  local status  = {}
 
-  reactorStatus            = getReactorStatus()
-  reactorPlasmaHeatPercent = formatPercent(reactor.getPlasmaHeat(), reactor.getMaxPlasmaHeat())
-  reactorProducing         = formatNumber(reactor.getProducing())
+  status["displayMaxEnergy"]   = formatNumber(matrix["maxEnergy"])
+  status["displayEnergy"]      = formatNumber(matrix["energy"])
+  status["displayTransferCap"] = formatNumber(matrix["transferCap"])
+  status["displayInput"]       = formatNumber(matrix["input"])
+  status["displayOutput"]      = formatNumber(matrix["output"])
+  
+  status["displayProducing"] = formatNumber(reactor["producing"])
+  
+  status["reactorStatus"]     = reactor["isIgnited"] and "Online" or "Offline"
+  status["scaledTransferCap"] = getScaledTransferCap(matrix["input"], matrix["output"], matrix["transferCap"])  
+  status["isPowerReactorOff"] = (status["reactorStatus"] == "Online") and (matrix["energy"] == matrix["maxEnergy"])
 
-  scaledTransferCap = getScaledTransferCap(matrix.getInput(), matrix.getOutput(), matrix.getTransferCap())  
+  status["matrix"]  = matrix
+  status["reactor"] = reactor
+
+  return status
 end
 
-local function drawStatus()
+local function drawStatus(status)
   local panelCount = 3  
   ui.setScreenPaddingPercent(5, 0, 0, 0)
   local panels = ui.getPanelDimensions(panelCount)
@@ -90,30 +107,32 @@ local function drawStatus()
 
   ui.drawScreenText(nil, 1, "Induction Matrix and Reactor Status", "center")
 
-  ui.drawHorizontalPercentageBar(panels[0]["x"], panels[0]["middle"], 1, colors.lime, true, colors.gray, true, matrix.getEnergy(), matrix.getMaxEnergy(), panels[0]["w"])
-  ui.drawHorizontalPercentageBar(panels[1]["x"], panels[1]["middle"]-1, 1, colors.lightblue, true, colors.gray, true, matrix.getInput(), scaledTransferCap, panels[1]["w"])
-  ui.drawHorizontalPercentageBar(panels[1]["x"], panels[1]["middle"]+1, 1, colors.yellow, true, colors.gray, true, matrix.getOutput(), scaledTransferCap, panels[1]["w"])
-  ui.drawHorizontalPercentageBar(panels[2]["x"], panels[2]["middle"], 1, colors.red, true, colors.gray, true, reactor.getPlasmaHeat(), reactor.getMaxPlasmaHeat(), panels[2]["w"])
+  ui.drawHorizontalPercentageBar(panels[0]["x"], panels[0]["middle"], 1, colors.lime, true, colors.gray, true, status["matrix"]["energy"], status["matrix"]["maxEnergy"], panels[0]["w"])
+  ui.drawHorizontalPercentageBar(panels[1]["x"], panels[1]["middle"]-1, 1, colors.lightblue, true, colors.gray, true, status["matrix"]["input"], status["scaledTransferCap"], panels[1]["w"])
+  ui.drawHorizontalPercentageBar(panels[1]["x"], panels[1]["middle"]+1, 1, colors.yellow, true, colors.gray, true, status["matrix"]["output"], status["scaledTransferCap"], panels[1]["w"])
+  ui.drawHorizontalPercentageBar(panels[2]["x"], panels[2]["middle"], 1, colors.red, true, colors.gray, true, status["reactor"]["plasmaHeat"], status["reactor"]["maxPlasmaHeat"], panels[2]["w"])
 
-  ui.drawPanelText(panels[0], nil, panels[0]["middle"]-1, "Energy Level: "..matrixEnergy.." RF", "left")    
-  ui.drawPanelText(panels[0], nil, panels[0]["middle"]+1, "Matrix Capacity: "..matrixMaxEnergy.." RF", "right")
+  ui.drawPanelText(panels[0], nil, panels[0]["middle"]-1, "Energy Level: "..status["displayEnergy"].." RF", "left")    
+  ui.drawPanelText(panels[0], nil, panels[0]["middle"]+1, "Matrix Capacity: "..status["displayMaxEnergy"].." RF", "right")
 
-  ui.drawPanelText(panels[1], nil, panels[1]["middle"]-2, "Input Rate: "..matrixInputRate.." RF/t", "left")
-  ui.drawPanelText(panels[1], nil, panels[1]["middle"], "Max Rate: "..matrixTransferCap.." RF/t", "right")
-  ui.drawPanelText(panels[1], nil, panels[1]["middle"]+2, "Output Rate: "..matrixOutputRate.." RF/t", "left")
+  ui.drawPanelText(panels[1], nil, panels[1]["middle"]-2, "Input Rate: "..status["displayInput"].." RF/t", "left")
+  ui.drawPanelText(panels[1], nil, panels[1]["middle"], "Max Rate: "..status["displayTransferCap"].." RF/t", "right")
+  ui.drawPanelText(panels[1], nil, panels[1]["middle"]+2, "Output Rate: "..status["displayOutput"].." RF/t", "left")
 
-  if reactor.isIgnited() then
-    ui.drawPanelText(panels[2], nil, panels[2]["middle"]-1, "Reactor Status:  "..reactorStatus, "left")
-    ui.drawPanelText(panels[2], nil, panels[2]["middle"]+1, "Reactor Production: "..reactorProducing.." RF/t", "left")
+  if status["reactor"]["isIgnited"] then
+    ui.drawPanelText(panels[2], nil, panels[2]["middle"]-1, "Reactor Status:  "..status["reactorStatus"], "left")
+    ui.drawPanelText(panels[2], nil, panels[2]["middle"]+1, "Reactor Production: "..status["displayProducing"].." RF/t", "left")
   end
 end
 
 local oldW, oldH = ui.setResolution(60, 20)
 
 while true do
-  setEnergyValues()
-  drawStatus()
-  handleMatrixFull()
+  local status = getMatrixReactorStatus()
+  drawStatus(status)
+  if status["isPowerReactorOff"] then
+    powerReactorOff()
+  end
 
   if keyboard.isControlDown() and keyboard.isKeyDown(keyboard.keys.w) then
     ui.setResolution(oldW, oldH)
